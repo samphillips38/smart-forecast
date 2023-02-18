f4dcf875b32a495aed96aea5b9774747f08a2194
Sam Phillips <srp60@cam.ac.uk>
2023-02-18 14:00:43 +0000

    Row like layout for show variable card

--- src/AddVariablePage/ShowVariableCard.js
+++ src/AddVariablePage/ShowVariableCard.js
@@ -4,7 +4,7 @@ import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import Button from '@mui/material/Button';
 import { Stack } from "@mui/system";
-import { CardContent } from "@material-ui/core";
+import { CardContent, Box } from "@material-ui/core";
 
 export default function ShowVariableCard({ variable, setIsEditing, onRemoveItem }) {
   const onEditClicked = () => {
@@ -12,24 +12,25 @@ export default function ShowVariableCard({ variable, setIsEditing, onRemoveItem 
   };
   return (
     <CardContent>
-        <Stack direction="row" justifyContent="space-between" alignItems="center">
-            <Stack>
-                <Typography variant="h7">{variable.title}</Typography>
-                <Typography variant="h7">Symbol Expression</Typography>
-            </Stack>
-            <Stack alignItems="flex-end" justifyContent="space-between">
+        <Stack spacing={2}>
+            <Stack direction="row" justifyContent="space-between">
+                <Typography variant="h4">{variable.title}, {variable.symbol}</Typography>
                 <IconButton
                     aria-label="delete"
                     onClick={() => onRemoveItem(variable)}
                 >
                     <CloseIcon fontSize="small" />
                 </IconButton>
+            </Stack>
+            <Typography variant="h6">Graphs will go here</Typography>
+            <Box alignSelf="flex-end">
                 <Button
+                    alignSelf="flex-end"
                     aria-label="Edit"
                     color="inherit"
                     onClick={onEditClicked}
                 >Edit Variable</Button>
-            </Stack>
+            </Box>
         </Stack>
     </CardContent>
   );
