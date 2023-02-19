52110ec27b6146c606da033f0c9baa849ea740f4
Sam Phillips <srp60@cam.ac.uk>
2023-02-19 19:44:11 +0000

    Added new format to addVariablePage

--- src/AddVariablePage/AddVariablePage.js
+++ src/AddVariablePage/AddVariablePage.js
@@ -5,6 +5,10 @@ import React, { useState, useEffect } from "react";
 import Stack from '@mui/material/Stack';
 import VariableCard from "./VariableCard";
 import AddVariableCard from "./AddVariableCard";
+import Typography from "@material-ui/core/Typography";
+import { Divider } from "@material-ui/core";
+import ConstantsGrid from "./ConstantsGrid/ConstantsGrid";
+import VariableGrid from "./VariableGrid/VariableGrid";
 
 export default function AddVariablePage({ data, setData }) {
     const onRemoveItem = (variable) => {
@@ -13,14 +17,21 @@ export default function AddVariablePage({ data, setData }) {
     };
     return (
         <Stack spacing={2}>
-            <OperatorBar/>
-            <VariableBar/>
-            {Object.entries(data).map(([key, value]) => (
+            <Typography h4>Constants</Typography>
+            <Divider/>
+            <ConstantsGrid data={data} setData={setData}></ConstantsGrid>
+            <Typography h4>Independent Variables</Typography>
+            <Divider/>
+            {/* <VariableGrid data={data} setData={setData} isDependent={false}/> */}
+            <Typography h4>Dependent Variables</Typography>
+            <Divider/>
+            {/* <VariableGrid data={data} setData={setData} isDependent={true}/> */}
+            {/* {Object.entries(data).map(([key, value]) => (
                 <VariableCard
                     variable={value}
                     onRemoveItem={onRemoveItem}
                 />
-            ))}
+            ))} */}
             <AddVariableCard data={data} setData={setData}/>
         </Stack>
     );
