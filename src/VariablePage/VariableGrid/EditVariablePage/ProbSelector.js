9b45137281365f8e455107a4a79691d2f674e496
Sam Phillips <srp60@cam.ac.uk>
2023-03-01 23:32:37 +0000

    Added grid to ProbSelector

--- src/VariablePage/VariableGrid/EditVariablePage/ProbSelector.js
+++ src/VariablePage/VariableGrid/EditVariablePage/ProbSelector.js
@@ -1,7 +1,29 @@
 import Typography from "@material-ui/core/Typography";
+import { Card, Grid } from "@material-ui/core";
+import { CardContent } from "@mui/material";
+
+function ProbCard({ index }) {
+    console.log(index);
+    return (
+        <Card>
+            <CardContent>
+                <Typography>This is card {index}</Typography>
+            </CardContent>
+        </Card>
+    );
+}
 
 export default function ProbSelector({ variable }) {
     return (
-        <Typography>Probabilistic Selector</Typography>
+        <>
+            <Typography>Probabilistic Selector</Typography>
+            <Grid container spacing={2}>
+                {[0, 1, 2, 3, 4, 5, 6].map((element) => (
+                    <Grid item>
+                        <ProbCard index={element} ></ProbCard>
+                    </Grid>
+                ))}
+            </Grid>
+        </>
     );
 }
