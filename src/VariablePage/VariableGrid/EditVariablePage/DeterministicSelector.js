d4df0f4e3af32cc82241a097d4c390c82f2446ed
Sam Phillips <srp60@cam.ac.uk>
2023-03-03 19:16:50 +0000

    Added table and graph to deterministic selector

--- src/VariablePage/VariableGrid/EditVariablePage/DeterministicSelector.js
+++ src/VariablePage/VariableGrid/EditVariablePage/DeterministicSelector.js
@@ -1,7 +1,39 @@
 import Typography from "@material-ui/core/Typography";
+import { Box, Grid, Table } from "@material-ui/core";
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableCell from "@mui/material/TableCell";
+import TableBody from '@mui/material/TableBody';
+import DetVariableChart from "../../../charts/DetVariableChart";
 
-export default function DeterministicSelector({ variable }) {
+export default function DeterministicSelector({ variable, timelineData }) {
+    console.log(`At DetSelector: ${timelineData}`);
     return (
-        <Typography>Deterministic Selector</Typography>
+        <Grid container>
+            <Grid item xs={12} md={6}>
+                <Table SX= {{ minWidth: 650 }} aria-label="Det Table">
+                    <TableHead>
+                        <TableRow >
+                            <TableCell align="right">Time</TableCell>
+                            <TableCell align="right">Value</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {timelineData.map((date, index) => (
+                            <TableRow>
+                                <TableCell 
+                                    align="right">{`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`}</TableCell>
+                                <TableCell align="right">{index}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </Grid>
+            <Grid item xs={12} md={6}>
+                <Box height={300}>
+                    <DetVariableChart variableData={variable.data}/>
+                </Box>
+            </Grid>
+        </Grid>
     );
 }
