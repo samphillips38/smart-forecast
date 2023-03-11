815996083df54ee01efbc7f9c532202e39aaa067
Sam Phillips <srp60@cam.ac.uk>
2023-03-11 15:53:16 +0000

    Created example probability card with children for inputs

--- src/VariablePage/VariableGrid/EditVariablePage/ProbSelector.js
+++ src/VariablePage/VariableGrid/EditVariablePage/ProbSelector.js
@@ -4,43 +4,33 @@ import { Grid, CardContent } from "@material-ui/core";
 import GaussianChart from "../../../charts/ExampleCharts/Gaussian";
 import TriangleChart from "../../../charts/ExampleCharts/Triangle";
 
-// function ProbCard({ child }) {
-//     return (
-//         <Card>
-//             <CardContent>
-//                 {...child}
-//             </CardContent>
-//         </Card>
-//     );
-// }
+function ProbCard({ children }) {
+    return (
+        <Grid item xs={6} sm={4} md={3}>
+            <Card>
+                <CardContent>
+                    {children}
+                </CardContent>
+            </Card>
+        </Grid>
+    );
+}
 
 export default function ProbSelector({ variable }) {
     return (
         <>
             <Typography>Probabilistic Selector</Typography>
             <Grid container spacing={2}>
-                <Grid item xs={6} sm={4} md={3}>
-                    <Card>
-                        <CardContent>
-                            <GaussianChart/>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid item xs={6} sm={4} md={3}>
-                    <Card>
-                        <CardContent>
-                            <TriangleChart/>
-                        </CardContent>
-                    </Card>
-                </Grid>
-                {[0, 1, 3].map((element) => (
-                    <Grid item xs={6} sm={4} md={3}>
-                        <Card>
-                            <CardContent>
-                                <GaussianChart/>
-                            </CardContent>
-                        </Card>
-                    </Grid>
+                <ProbCard>
+                    <GaussianChart/>
+                </ProbCard>
+                <ProbCard>
+                    <TriangleChart/>
+                </ProbCard>
+                {[0, 1, 2, 3].map((element) => (
+                    <ProbCard>
+                        {element % 2 ? (<TriangleChart/>) : (<GaussianChart/>)}
+                    </ProbCard>
                 ))}
             </Grid>
         </>
