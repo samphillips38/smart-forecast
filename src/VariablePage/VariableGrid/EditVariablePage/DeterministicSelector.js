dd00f71578e4a256dbdef4d34c31e0c01180bc2c
Sam Phillips <srp60@cam.ac.uk>
2023-03-11 14:31:36 +0000

    Removed timeline. Cleaning up det selector tab

--- src/VariablePage/VariableGrid/EditVariablePage/DeterministicSelector.js
+++ src/VariablePage/VariableGrid/EditVariablePage/DeterministicSelector.js
@@ -1,39 +1,17 @@
-import Typography from "@material-ui/core/Typography";
-import { Box, Grid, Table } from "@material-ui/core";
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import TableCell from "@mui/material/TableCell";
-import TableBody from '@mui/material/TableBody';
+import { Box, } from "@material-ui/core";
 import DetVariableChart from "../../../charts/DetVariableChart";
+import Button from '@mui/material/Button';
 
-export default function DeterministicSelector({ variable, timelineData }) {
-    console.log(`At DetSelector: ${timelineData}`);
+export default function DeterministicSelector({ variable }) {
+    const onAddPoint = () => {
+        
+    }
     return (
-        <Grid container>
-            <Grid item xs={12} md={6}>
-                <Table SX= {{ minWidth: 650 }} aria-label="Det Table">
-                    <TableHead>
-                        <TableRow >
-                            <TableCell align="right">Time</TableCell>
-                            <TableCell align="right">Value</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {timelineData.map((date, index) => (
-                            <TableRow>
-                                <TableCell 
-                                    align="right">{`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`}</TableCell>
-                                <TableCell align="right">{index}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <Box height={300}>
-                    <DetVariableChart variableData={variable.data}/>
-                </Box>
-            </Grid>
-        </Grid>
+        <>
+            <Box height={350}>
+                <DetVariableChart variableData={variable.data}/>
+            </Box>
+            <Button onClick={onAddPoint} >Add Point</Button>
+        </>
     );
 }
