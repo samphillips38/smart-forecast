937ef4479f0cf1313bfee60be6b4bffda5e1c797
Sam Phillips <srp60@cam.ac.uk>
2023-03-11 14:15:41 +0000

    Edit values on detvariable graph by clicking points

--- src/charts/DetVariableChart.js
+++ src/charts/DetVariableChart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   LineChart,
   Line,
@@ -9,36 +9,85 @@ import {
   Legend,
   ResponsiveContainer
 } from "recharts";
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
+import Dialog from '@mui/material/Dialog';
+import { DialogContent } from "@material-ui/core";
+import Stack from '@mui/material/Stack';
+import TextField from "@mui/material/TextField";
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Button from '@mui/material/Button';
+
+function EditValue({ onClose, open, variableData }) {
+    const onSaveClicked = () => {
+        onClose();
+    }
+    const onCancelClicked = () => {
+        onClose();
+    }
+    return (
+        <Dialog onClose={onClose} open={open}>
+            <DialogContent>
+                <Stack spacing={2} width="200px">
+                    <Typography >Edit Point</Typography>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DatePicker label="Select Date" />
+                    </LocalizationProvider>
+                    <TextField
+                        id="Val"
+                        label="Select Value"
+                        defaultValue={variableData.mean[1]}
+                        onChange={()=>{}}
+                        fullWidth
+                    />
+                    <Stack direction="row" justifyContent="space-between">
+                        <Button onClick={onCancelClicked}>Cancel</Button>
+                        <Button onClick={onSaveClicked}>Save</Button>
+                    </Stack>
+                </Stack>
+            </DialogContent>
+        </Dialog>
+    )
+}
 
 export default function DetVariableChart({ variableData }) {
-  const data = variableData["time"].map((t, i) => ({
-    name: t,
-    mean: variableData["mean"][i]
-  }));
-  return (
-    <ResponsiveContainer>
-      <LineChart
-        data={data}
-        margin={{
-          top: 0,
-          right: 0,
-          left: 0,
-          bottom: 0
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        {/* <Legend /> */}
-        {/* <Line
-          type="monotone"
-          dataKey="pv"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        /> */}
-        <Line type="monotone" dataKey="mean" stroke="#82ca9d" />
-      </LineChart>
-    </ResponsiveContainer>
-  );
+    const [open, setOpen] = useState(false);
+    const handleClick = (event) => {
+        console.log("clicked point (chart)", event.activePayload[0].payload);
+        setOpen(true)
+    };
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+    
+    const data = variableData["time"].map((t, i) => ({
+        name: t,
+        mean: variableData["mean"][i]
+    }));
+    return (
+        <>
+        <ResponsiveContainer>
+        <LineChart
+            data={data}
+            margin={{
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: 0
+            }}
+            onClick={handleClick}
+        >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Line type="monotone" dataKey="mean" stroke="#82ca9d"/>
+        </LineChart>
+        </ResponsiveContainer>
+        <EditValue onClose={handleClose} open={open} variableData={variableData}/>
+        </>
+    );
 }
