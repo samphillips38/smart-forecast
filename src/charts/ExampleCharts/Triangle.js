afdd0bbe14e78b6741e1324e928880e33d215e7f
Sam Phillips <srp60@cam.ac.uk>
2023-03-11 15:34:59 +0000

    Added triangle distribution

---
+++ src/charts/ExampleCharts/Triangle.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { AreaChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Area } from 'recharts';
+
+function getGaussianData() {
+    const probabilityDensityData = [];
+    let min = -5;
+    let max = 5;
+    let top = 2;
+    let TopValue = 2 / (max - min);
+    
+    for (let i = min; i <= max; i += 0.1) {
+        const probabilityDensity = i < top ? TopValue * (i - min) / (top - min) : TopValue * (max - i) / (max - top);
+        probabilityDensityData.push({ x: i.toFixed(2), y: probabilityDensity });
+    }
+    return probabilityDensityData;
+}
+
+export default function TriangleChart() {
+    const probabilityDensityData = getGaussianData()
+    return (
+        <ResponsiveContainer width="100%" height={150}>
+            <AreaChart data={probabilityDensityData}>
+                <Area type="monotone" dataKey="y" stroke="#8884d8" fill="#8884d8" />
+                <Line type="linear" dataKey="y" stroke="#ff7300" />
+            </AreaChart>
+        </ResponsiveContainer>
+    );
+}
