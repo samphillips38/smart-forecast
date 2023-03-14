05a65f0d0e002ad5176ed92f77c7934024c29cc6
Sam Phillips <srp60@cam.ac.uk>
2023-03-14 07:34:32 +0000

    Attempting new drawer configuration, changing depending on screen width

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -16,6 +16,10 @@ import ExploreIcon from "@material-ui/icons/Explore";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import SettingsIcon from "@material-ui/icons/Settings";
 import CalculateIcon from "@mui/icons-material/Calculate";
+import ListItemButton from '@mui/material/ListItemButton';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+
 
 const drawerWidth = 240;
 
@@ -36,80 +40,107 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export default function ({ open, handleDrawerClose, setContent }) {
-  const classes = useStyles();
+export default function ({ window, open, setOpen, handleDrawerClose, setContent }) {
+    // const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  return (
-    <Drawer
-    className={classes.drawer}
-    variant="persistent"
-    anchor="left"
-    open={open}
-    classes={{
-        paper: classes.drawerPaper
-    }}
-    >
-    <div className={classes.drawerHeader}>
-        <IconButton onClick={handleDrawerClose}>
-        <ChevronLeftIcon />
-        </IconButton>
-    </div>
-    <Divider />
-    <div className="">
-        <List>
-            <ListItem button onClick={() => setContent("Dashboard")}>
-                <ListItemIcon>
-                    <DashboardIcon />
-                </ListItemIcon>
-                <ListItemText primary="Dashboard" />
-            </ListItem>
-            <ListItem button onClick={() => setContent("Add Variable")}>
-                <ListItemIcon>
-                    <CalculateIcon />
-                </ListItemIcon>
-                <ListItemText primary="Add Variable" />
-            </ListItem>
-            <ListItem button>
-                <ListItemIcon>
-                    <MailIcon />
-                </ListItemIcon>
-                <ListItemText primary="Scenarios" />
-            </ListItem>
-            <ListItem button>
-                <ListItemIcon>
-                    <EventIcon />
-                </ListItemIcon>
-                <ListItemText primary="Optimisations" />
-            </ListItem>
-            <ListItem button>
-                <ListItemIcon>
-                    <ExploreIcon />
-                </ListItemIcon>
-                <ListItemText primary="Sensitivity Analysis" />
-            </ListItem>
-            <ListItem button>
-                <ListItemIcon>
-                    <FavoriteIcon />
-                </ListItemIcon>
-                <ListItemText primary="Favorites" />
-            </ListItem>
-        </List>
-        <Divider />
-        <List>
-            <ListItem button>
-                <ListItemIcon>
-                    <DevicesIcon />
-                </ListItemIcon>
-                <ListItemText primary="Devices" />
-            </ListItem>
-            <ListItem button>
-                <ListItemIcon>
-                    <SettingsIcon />
-                </ListItemIcon>
-                <ListItemText primary="Settings" />
-            </ListItem>
-        </List>
-    </div>
-    </Drawer>
-  );
+    // const handleDrawerToggle = () => {
+    //     setMobileOpen(!mobileOpen);
+    // };
+
+    const menuItemList = (
+        <div>
+            <Toolbar />
+            <Divider />
+            <List>
+                <ListItem button onClick={() => setContent("Dashboard")}>
+                    <ListItemIcon>
+                        <DashboardIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Dashboard" />
+                </ListItem>
+                <ListItem button onClick={() => setContent("Add Variable")}>
+                    <ListItemIcon>
+                        <CalculateIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Add Variable" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <MailIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Scenarios" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <EventIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Optimisations" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <ExploreIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Sensitivity Analysis" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <FavoriteIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Favorites" />
+                </ListItem>
+            </List>
+            <Divider />
+            <List>
+                <ListItem button>
+                    <ListItemIcon>
+                        <DevicesIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Devices" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <SettingsIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Settings" />
+                </ListItem>
+            </List>
+        </div>
+    )
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+
+    return (
+        <Box
+            component="nav"
+            sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+            aria-label="mailbox folders"
+        >
+            {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+            <Drawer
+                container={container}
+                variant="temporary"
+                open={open}
+                onClose={handleDrawerClose}
+                ModalProps={{
+                    keepMounted: true, // Better open performance on mobile.
+                }}
+                sx={{
+                    display: { xs: 'block', sm: 'none' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                }}
+            >
+                {menuItemList}
+            </Drawer>
+            <Drawer
+                variant="permanent"
+                sx={{
+                    display: { xs: 'none', sm: 'block' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                }}
+                open
+            >
+                {menuItemList}
+            </Drawer>
+        </Box>
+    );
 }
