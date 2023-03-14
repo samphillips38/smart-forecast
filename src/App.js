1c2784ea1af50baaf80379a3d729e52b11776f18
Sam Phillips <srp60@cam.ac.uk>
2023-03-14 08:24:56 +0000

    Content width and sidebar layout now works regardless of screen size

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
-import { CssBaseline, ThemeProvider, createTheme } from "@material-ui/core";
+import { CssBaseline, ThemeProvider, createTheme, Toolbar } from "@material-ui/core";
 import Content from "./Content";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import { getData, formatData } from "./Utility";
 import { Box } from "@material-ui/core";
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const loadedData = getData();
 const drawerWidth = 240;
@@ -43,56 +44,60 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function App() {
-  const classes = useStyles();
-  const [open, setOpen] = useState(false);
-  const [darkMode, setDarkMode] = useState(false);
-  const [data, setData] = useState({});
-  const [content, setContent] = useState("Dashboard");
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+    const [darkMode, setDarkMode] = useState(false);
+    const [data, setData] = useState({});
+    const [content, setContent] = useState("Dashboard");
+    const theme = createTheme({
+        palette: {
+            type: darkMode ? "dark" : "light"
+        }
+    });
+    const isMobileSize = !useMediaQuery(theme.breakpoints.up('sm'));
+    
+    useEffect(() => {
+        setData(formatData(loadedData));
+    }, []);
 
-  useEffect(() => {
-    setData(formatData(loadedData));
-  }, []);
+    const handleDrawerToggle = () => {
+        setOpen(!open);
+    };
 
-  const handleDrawerToggle = () => {
-    setOpen(!open);
-  };
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
-  const theme = createTheme({
-    palette: {
-      type: darkMode ? "dark" : "light"
-    }
-  });
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
-  return (
-    <ThemeProvider theme={theme}>
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <Header
-            handleDrawerToggle={handleDrawerToggle}
-            toggleDarkMode={toggleDarkMode}
-            darkMode={darkMode}
-            />
-            <Sidebar
-            handleDrawerClose={handleDrawerClose}
-            open={open}
-            setOpen={setOpen}
-            setContent={setContent}
-            />
-            <Box
-                component="main"
-                sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-            >
-                <div className={classes.drawerHeader} />
-                <Content data={data} setData={setData} content={content} />
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+    };
+    return (
+        <ThemeProvider theme={theme}>
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <Header
+                handleDrawerToggle={handleDrawerToggle}
+                toggleDarkMode={toggleDarkMode}
+                darkMode={darkMode}
+                />
+                <Sidebar
+                handleDrawerClose={handleDrawerClose}
+                open={open}
+                setOpen={setOpen}
+                setContent={setContent}
+                isMobileSize={isMobileSize}
+                />
+                <Box
+                    component="main"
+                    sx={{ flexGrow: 1, p: 3, 
+                        width: isMobileSize ? '100%' : { 
+                            sm: `calc(100% - ${drawerWidth}px)` 
+                        } }}
+                >
+                    <Toolbar/>
+                    <Content data={data} setData={setData} content={content} />
+                </Box>
             </Box>
-        </Box>
-    </ThemeProvider>
-  );
+        </ThemeProvider>
+    );
 }
