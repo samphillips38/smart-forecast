cb482c838cb8313f43eebb3b52b79d14733fcfb9
Sam Phillips <srp60@cam.ac.uk>
2023-03-15 07:37:35 +0000

    Change double graph width. Refactored name

--- src/VariablePage/VariableGrid/ShowVariableCard.js
+++ src/VariablePage/VariableGrid/ShowVariableCard.js
@@ -1,12 +1,12 @@
 import React from "react";
-import EditableGraph from "./EditableGraph";
+import DoubleGraph from "./DoubleGraph";
 import DetVariableChart from "../../charts/DetVariableChart";
 
 export default function ShowVariableCard({ variable, editedVariable, setEditedVariable, onRemoveItem }) {
   return (
     <>
         {variable.isProb ? (
-            <EditableGraph editedVariable={editedVariable} setEditedVariable={setEditedVariable}/>
+            <DoubleGraph editedVariable={editedVariable} setEditedVariable={setEditedVariable}/>
         ) : (
             <DetVariableChart variableData={editedVariable.data} width="100%" height={350}/>
         )}
