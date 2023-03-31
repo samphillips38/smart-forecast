f8927b9cd2733e90f9739f33e6b8b5f5b749eec4
Sam Phillips <srp60@cam.ac.uk>
2023-04-01 00:25:31 +0100

    Removed remaining non redux state data from App.js. Logic for more compatible header for phones

--- src/Header.js
+++ src/Header.js
@@ -10,6 +10,11 @@ import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import Badge from "@material-ui/core/Badge";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import { makeStyles } from "@material-ui/core/styles";
+import { useSelector } from "react-redux";
+import Stack from '@mui/material/Stack';
+
+import { selectDisplayingInvestment } from "./investmentsReducer";
+import { Button } from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
   appbar: {
@@ -28,58 +33,70 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Header({
-  handleDrawerToggle,
-  toggleDarkMode,
-  darkMode
-}) {
-  const classes = useStyles();
-  return (
-    <AppBar position="fixed" className={classes.appbar}>
-      <Toolbar>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          onClick={handleDrawerToggle}
-          edge="start"
-          sx={{
-            display: { xs: 'block', sm: 'none' }
-            }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" noWrap>
-          Smart Forecast
-        </Typography>
-        <div className={classes.spacer} />
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          onClick={toggleDarkMode}
-          edge="start"
-          className={classes.rightIcons}
-        >
-          {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
-        </IconButton>
+    isMobileSize,
+    handleDrawerToggle,
+    toggleDarkMode,
+    darkMode
+    }) {
+    const classes = useStyles();
+    const investment = useSelector(selectDisplayingInvestment);
+    return (
+        <AppBar position="fixed" className={classes.appbar}>
+        <Toolbar>
+            {isMobileSize && (
+                <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerToggle}
+                edge="start"
+                sx={{
+                    display: { xs: 'block', sm: 'none' }
+                    }}
+                >
+                        <MenuIcon />
+                </IconButton>
+            )}
+            <Stack spacing={2} direction="row">
+                <Typography variant="h6" noWrap>
+                {isMobileSize ? "SF" : "Smart Forecast"}
+                </Typography>
+                {investment && investment.status == "idle" && (
+                    <Button 
+                    color="inherit" 
+                    variant="outlined"
+                    display={{marginLeft: 0.5}}>Refresh</Button>
+                )}
+            </Stack>
+            <div className={classes.spacer} />
+            <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDarkMode}
+            edge="start"
+            className={classes.rightIcons}
+            >
+            {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
 
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge="start"
-          className={classes.rightIcons}
-        >
-          <Badge badgeContent={4} color="secondary">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge="start"
-          className={classes.rightIcons}
-        >
-          <AccountCircleIcon />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
-  );
+            <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            className={classes.rightIcons}
+            >
+            <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon />
+            </Badge>
+            </IconButton>
+            <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            className={classes.rightIcons}
+            >
+            <AccountCircleIcon />
+            </IconButton>
+        </Toolbar>
+        </AppBar>
+    );
 }
