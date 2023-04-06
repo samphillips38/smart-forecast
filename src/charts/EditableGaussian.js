f6bd6210810ac6606e05014e99bf71557a479285
Sam Phillips <srp60@cam.ac.uk>
2023-04-06 15:13:57 +0100

    Allow editing for values on probabilistic graph

--- src/charts/EditableGaussian.js
+++ src/charts/EditableGaussian.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   LineChart,
   Line,
@@ -21,8 +21,24 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import Button from '@mui/material/Button';
 import GaussianChart from "./ExampleCharts/Gaussian";
 
-function EditValue({ onClose, open, variableData }) {
+function EditValue({ onClose, open, editedVariable, setEditedVariable, i }) {
+    const [mean, setMean] = useState(editedVariable.data.mean[i]);
+    const [stDev, setStDev] = useState(editedVariable.data.std[i]);
+    const [time, setTime] = useState(editedVariable.data.time[i]);
+    useEffect(() => {
+        setMean(editedVariable.data.mean[i]);
+        setStDev(editedVariable.data.std[i]);
+        setTime(editedVariable.data.time[i]);
+    }, [i])
     const onSaveClicked = () => {
+        setEditedVariable({
+            ...editedVariable,
+            data: {
+                time: editedVariable.data.time.map((el, index) => (index == i) ? time : el),
+                mean: editedVariable.data.mean.map((el, index) => (index == i) ? parseFloat(mean) : el),
+                std: editedVariable.data.std.map((el, index) => (index == i) ? parseFloat(stDev) : el)
+            }
+        })
         onClose();
     }
     const onCancelClicked = () => {
@@ -40,15 +56,15 @@ function EditValue({ onClose, open, variableData }) {
                         <TextField
                             id="mean"
                             label="Select Mean"
-                            defaultValue={variableData.mean[1]}
-                            onChange={()=>{}}
+                            defaultValue={mean}
+                            onChange={(e) => setMean(e.target.value)}
                             fullWidth
                         />
                         <TextField
                             id="std"
                             label="Select Standard Deviation"
-                            defaultValue={variableData.std[1]}
-                            onChange={()=>{}}
+                            defaultValue={stDev}
+                            onChange={(e) => setStDev(e.target.value)}
                             fullWidth
                         />
                     </Stack>
@@ -63,24 +79,26 @@ function EditValue({ onClose, open, variableData }) {
     )
 }
 
-export default function EditableGaussian({ variableData }) {
+export default function EditableGaussian({ editedVariable, setEditedVariable }) {
     const [open, setOpen] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
     const handleClick = (event) => {
+        setSelectedIndex(event.activeTooltipIndex)
         setOpen(true)
     };
     const handleClose = () => {
         setOpen(false);
     };
-    const data = variableData["time"].map((t, i) => ({
+    const formattedData = editedVariable.data.time.map((t, i) => ({
         name: t,
-        mean: variableData["mean"][i],
-        'std+': (variableData.std && i < variableData.std.length) ? variableData.mean[i] + variableData.std[i] : null,
-        'std-': (variableData.std && i < variableData.std.length) ? variableData.mean[i] - variableData.std[i] : null
+        mean: editedVariable.data.mean[i],
+        'std+': (editedVariable.data.std && i < editedVariable.data.std.length) ? editedVariable.data.mean[i] + editedVariable.data.std[i] : null,
+        'std-': (editedVariable.data.std && i < editedVariable.data.std.length) ? editedVariable.data.mean[i] - editedVariable.data.std[i] : null
     }));
     return (
         <>
         <ResponsiveContainer width="100%" height={300}>
-            <LineChart data={data} onClick={handleClick}>
+            <LineChart data={formattedData} onClick={handleClick}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" height={20}/>
                 <YAxis width={30}/>
@@ -90,7 +108,13 @@ export default function EditableGaussian({ variableData }) {
                 <Line type="monotone" dataKey="std-" stroke="#ffc658" />
             </LineChart>
         </ResponsiveContainer>
-        <EditValue onClose={handleClose} open={open} variableData={variableData}/>
+        <EditValue 
+            onClose={handleClose} 
+            open={open} 
+            editedVariable={editedVariable}
+            setEditedVariable={setEditedVariable}
+            i={selectedIndex}
+            />
         </>
     );
 }
