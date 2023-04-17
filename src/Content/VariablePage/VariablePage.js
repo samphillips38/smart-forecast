8a5e1dddf6f1842c789d1659aa3319757955a714
Sam Phillips <srp60@cam.ac.uk>
2023-04-17 21:10:11 +0200

    Started building out VariablePage

--- src/Content/VariablePage/VariablePage.js
+++ src/Content/VariablePage/VariablePage.js
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 import Stack from '@mui/material/Stack';
+import { useSelector } from "react-redux";
+import { Grid } from "@material-ui/core";
 
+import { selectVariables } from "../../modelsReducer";
 import VariableTopBar from "./VariableTopBar";
-import ShowVariable from "./VariableDialog";
+import VariableDialog from "./VariableDialog";
+import VariableCard from "../../Components/VariableCard";
 
 export default function VariablePage() {
+    const variables = useSelector(selectVariables);
     const [showVariableIsOpen, setShowVariableIsOpen] = useState(false);
     return (
         <Stack spacing={2} >
             <VariableTopBar/>
+            <Grid container>
+                {variables.map((variable) => (
+                    <Grid item id={variable.id}>
+                        <VariableCard variable={variable}/>
+                        <VariableDialog variable={variable} open={open} setOpen={setOpen}/>
+                    </Grid>
+                ))}
+            </Grid>
         </Stack>
     );
 }
