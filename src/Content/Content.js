ee6398afc1327b35fc9f7d62f9519c9ff81b9173
Sam Phillips <srp60@cam.ac.uk>
2023-04-17 19:48:25 +0200

    Better imports in Content.js. Moved Dashboard.js

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+
 import Dashboard from "./Dashboard/Dashboard";
 import VariablePage from "./VariablePage/VariablePage";
 
