8a2ae997514f1f481c0b77c4b634c411b02bfb46
Sam Phillips <srp60@cam.ac.uk>
2023-04-17 19:40:07 +0200

    Restructured App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from "react";
-import clsx from "clsx";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useState } from "react";
 import { CssBaseline, ThemeProvider, createTheme, Toolbar } from "@material-ui/core";
 import Content from "./Content";
 import Header from "./Header";
@@ -13,39 +11,7 @@ import store from "./store";
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex"
-  },
-
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    marginLeft: -drawerWidth
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end"
-  }
-}));
-
 export default function App() {
-    const classes = useStyles();
     const [open, setOpen] = useState(false);
     const [darkMode, setDarkMode] = useState(false);
     const [content, setContent] = useState("Dashboard");
@@ -82,16 +48,15 @@ export default function App() {
                         <Sidebar
                         handleDrawerClose={handleDrawerClose}
                         open={open}
-                        setOpen={setOpen}
                         setContent={setContent}
                         isMobileSize={isMobileSize}
                         />
                         <Box
-                            component="main"
-                            sx={{ flexGrow: 1, p: 3, 
-                                width: isMobileSize ? '100%' : { 
-                                    sm: `calc(100% - ${drawerWidth}px)` 
-                                } }}
+                        component="main"
+                        sx={{ flexGrow: 1, p: 3, 
+                            width: isMobileSize ? '100%' : { 
+                                sm: `calc(100% - ${drawerWidth}px)` 
+                            } }}
                         >
                             <Toolbar/>
                             <Content content={content} />
